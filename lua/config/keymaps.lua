b21d8a46050395d7b171431133a4db3a027530ae
betoxxD <betoxxdiosnava@gmail.com>
2025-01-20 10:56:55 -0600

    New configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,5 +12,9 @@ vim.keymap.set(
 -- Compile and run C/C++ code in a split terminal
 vim.keymap.set("n", "<leader>fw", function()
   vim.cmd("w")
-  vim.cmd("terminal g++ -std=c++17 -Wall -Wextra -O2 -o %:r % && ./" .. vim.fn.expand("%:r"))
+  vim.cmd("split | terminal g++ -std=c++17 -Wall -Wextra -O2 -o %:r % && ./" .. vim.fn.expand("%:r"))
 end, { noremap = true, silent = true, desc = "Compile and run C/C++ code" })
+
+-- Move the current line up or down
+vim.keymap.set("n", "<leader>j", ":m .+1<CR>==", { noremap = true, silent = true, desc = "Move the current line down" })
+vim.keymap.set("n", "<leader>k", ":m .-2<CR>==", { noremap = true, silent = true, desc = "Move the current line up" })
